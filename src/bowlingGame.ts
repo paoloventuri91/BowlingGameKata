c327a3d53e80a18f8f608cade118fad49a2d05a6
Paolo Venturi <paolo.venturi@cgm.com>
2021-09-23 15:01:17 +0200

    Refactoring: getBonus extracted

--- src/bowlingGame.ts
+++ src/bowlingGame.ts
@@ -18,16 +18,18 @@ export class BowlingGame {
       score.addValue(roll.pins);
       frame.addRoll(roll);
 
-      if (frame.score.canIHaveBonus()) {
-        if (frame.isStrike()) score.addValue(bowlingRolls.getBonus(2).value);
-        else score.addValue(bowlingRolls.getBonus(1).value);
-      }
+      if (frame.score.canIHaveBonus())
+        score.addValue(BowlingGame.getBonus(frame, bowlingRolls));
 
-      if (frame.isComplete()) {
-        frame = new Frame();
-      }
+      if (frame.isComplete()) frame = new Frame();
     }
 
     return score.value;
+  }
+
+  private static getBonus(frame: Frame, bowlingRolls: BowlingRolls): number {
+    if (frame.isStrike()) return bowlingRolls.getBonus(2).value;
+
+    return bowlingRolls.getBonus(1).value;
   }
 }
