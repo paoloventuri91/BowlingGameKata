0375abc86ee7793f4647a5651a935809383a7467
Paolo Venturi <paolo.venturi@cgm.com>
2021-09-23 12:09:33 +0200

    A roll by 1 returns 1: red

--- src/tests/bowling-score.test.ts
+++ src/tests/bowling-score.test.ts
@@ -3,3 +3,7 @@ import { BowlingGame } from "./../BowlingGame";
 test("A roll by 0 returns 0", () => {
   expect(BowlingGame.calculateScore([0])).toBe(0);
 });
+
+test("A roll by 1 returns 1", () => {
+  expect(BowlingGame.calculateScore([1])).toBe(1);
+});
